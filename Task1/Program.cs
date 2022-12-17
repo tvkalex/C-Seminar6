f1a2f5c0d690b90290f7764d9cf0fae78a2d5bc3
tvkalex <tvkalex@yandex.ru>
2022-12-18 01:10:39 +0300

    Add Seminar6Task1 solution

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пользователь вводит с клавиатуры М чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+Console.Write("Введите положительные и отрицательные числа через запятую: ");
+int[] arr = Array.ConvertAll(Console.ReadLine()!.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов > 0: {count}");
